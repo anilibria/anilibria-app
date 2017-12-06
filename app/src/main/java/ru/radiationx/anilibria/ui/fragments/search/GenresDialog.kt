88c7189d7b672fcd98e43069bbb228d8732cea9f
Puls7289 <31839997+Puls7289@users.noreply.github.com>
2017-12-06 17:16:18 +0500

    some changes

--- app/src/main/java/ru/radiationx/anilibria/ui/fragments/search/GenresDialog.kt
+++ app/src/main/java/ru/radiationx/anilibria/ui/fragments/search/GenresDialog.kt
@@ -2,63 +2,62 @@ package ru.radiationx.anilibria.ui.fragments.search
 
 import android.content.Context
 import android.support.design.widget.BottomSheetDialog
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.TextView
 import ru.radiationx.anilibria.ui.adapters.BaseAdapter
 import ru.radiationx.anilibria.ui.adapters.BaseViewHolder
 
 /**
  * Created by mintrocket on 04.12.2017.
  */
-class GenresDialog(context: Context, listener: ClickListener) {
+class GenresDialog(context: Context, private val listener: ClickListener) {
     private val dialog: BottomSheetDialog = BottomSheetDialog(context)
-    private val listView:ListView = ListView(context)
-    private val adapter: ArrayAdapter<String> = ArrayAdapter(context, android.R.layout.select_dialog_item)
-
-    init {
-        listView.adapter = adapter
-        listView.setOnItemClickListener { adapterView, view, i, l ->
-            run {
-                dialog.dismiss()
-                listener.onItemClick(adapter.getItem(i))
-            }
-        }
+    private val adapter: ReleasesAdapter = ReleasesAdapter()
+    private val recyclerView: RecyclerView = RecyclerView(context).apply {
+        setHasFixedSize(true)
+        layoutManager = LinearLayoutManager(this.context)
+        adapter = adapter
     }
 
-    fun setItems(items:List<String>){
-        adapter.addAll(items)
+    fun setItems(items: List<String>) {
+        adapter.bindItems(items)
         adapter.notifyDataSetChanged()
     }
 
-    fun showDialog(){
-        listView.parent?.let {
-            (it as ViewGroup).removeView(listView)
+    fun showDialog() {
+        recyclerView.parent?.let {
+            (it as ViewGroup).removeView(recyclerView)
         }
-        dialog.setContentView(listView)
+        dialog.setContentView(recyclerView)
         dialog.show()
     }
 
-    interface ClickListener{
+    interface ClickListener {
         fun onItemClick(item: String)
     }
 
-    /*class ReleasesAdapter : BaseAdapter<String, BaseViewHolder<*>>(){
-        override fun onBindViewHolder(holder: BaseViewHolder<*>?, position: Int) {
-
+    inner class ReleasesAdapter : BaseAdapter<String, ReleasesAdapter.GenreHolder>() {
+        override fun onBindViewHolder(holder: ReleasesAdapter.GenreHolder?, position: Int) {
+            holder?.bind(items[position])
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BaseViewHolder<*> {
-
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ReleasesAdapter.GenreHolder {
+            return GenreHolder(inflateLayout(parent, android.R.layout.simple_selectable_list_item))
         }
 
-        private inner class LoadMoreHolder internal constructor(itemView: View) : BaseViewHolder<String>(itemView) {
+        inner class GenreHolder internal constructor(itemView: View) : BaseViewHolder<String>(itemView) {
+            init {
+                itemView.setOnClickListener {
+                    listener.onItemClick(items[layoutPosition])
+                }
+            }
 
-
-            override fun bind(position: Int) {
+            override fun bind(item: String) {
+                itemView.findViewById<TextView>(android.R.id.text1).text = item
             }
         }
-
-    }*/
+    }
 }

78708286d17b00c9feb7c19e57649634d7553e46
Evgeny Nizamiev <radiationx@yandex.ru>
2018-10-07 19:08:36 +0500

    kitkat fixes

--- app/src/main/java/ru/radiationx/anilibria/extension/Context.kt
+++ app/src/main/java/ru/radiationx/anilibria/extension/Context.kt
@@ -2,13 +2,29 @@ package ru.radiationx.anilibria.extension
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.support.annotation.AttrRes
 import android.support.annotation.ColorInt
+import android.support.annotation.DrawableRes
 import android.support.annotation.StyleRes
+import android.support.v4.content.ContextCompat
+import android.support.v7.content.res.AppCompatResources
 import android.util.TypedValue
 import biz.source_code.miniTemplator.MiniTemplator
 import ru.radiationx.anilibria.R
 import ru.radiationx.anilibria.model.data.holders.AppThemeHolder
+
+@DrawableRes
+fun Context.getDrawableResAttr(@AttrRes attr: Int): Int {
+    val a = this.theme.obtainStyledAttributes(intArrayOf(attr))
+    val attributeResourceId = a.getResourceId(0, 0)
+    a.recycle()
+    return attributeResourceId
+}
+
+fun Context.getDrawableAttr(@AttrRes attr: Int): Drawable {
+    return ContextCompat.getDrawable(this, getDrawableResAttr(attr))!!
+}
 
 @ColorInt
 fun Context.getColorFromAttr(@AttrRes attr: Int): Int {
--- app/src/main/java/ru/radiationx/anilibria/ui/widgets/bbwidgets/BbQuote.kt
+++ app/src/main/java/ru/radiationx/anilibria/ui/widgets/bbwidgets/BbQuote.kt
@@ -5,6 +5,7 @@ import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.widget.TextView
 import ru.radiationx.anilibria.R
+import ru.radiationx.anilibria.extension.getDrawableAttr
 
 /**
  * Created by radiationx on 21.01.18.
@@ -23,7 +24,7 @@ class BbQuote @JvmOverloads constructor(
                 (8 * dens).toInt(),
                 (8 * dens).toInt()
         )
-        background = ContextCompat.getDrawable(context, R.drawable.bg_bb_quote)
+        background = context.getDrawableAttr(R.attr.bg_comment_quote)
 
         val title = TextView(context)
         title.text = "Цитата"

dcc233e0966fbd9a8b8f8b45edae281c32b219c0
Evgeny Nizamiev <radiationx@yandex.ru>
2019-02-24 18:02:37 +0500

    revert checker

--- app/src/main/java/ru/radiationx/anilibria/ui/activities/main/MainActivity.kt
+++ app/src/main/java/ru/radiationx/anilibria/ui/activities/main/MainActivity.kt
@@ -1,11 +1,17 @@
 package ru.radiationx.anilibria.ui.activities.main
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.app.Fragment
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.GridLayoutManager
 import android.util.Log
 import com.arellomobile.mvp.presenter.InjectPresenter
@@ -13,18 +19,24 @@ import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.nostra13.universalimageloader.core.ImageLoader
 import kotlinx.android.synthetic.main.activity_container.*
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.radiationx.anilibria.App
+import ru.radiationx.anilibria.BuildConfig
 import ru.radiationx.anilibria.R
 import ru.radiationx.anilibria.di.extensions.getDependency
 import ru.radiationx.anilibria.di.extensions.injectDependencies
+import ru.radiationx.anilibria.entity.app.updater.UpdateData
 import ru.radiationx.anilibria.entity.common.AuthState
 import ru.radiationx.anilibria.extension.getMainStyleRes
 import ru.radiationx.anilibria.model.data.holders.AppThemeHolder
 import ru.radiationx.anilibria.model.system.messages.SystemMessenger
 import ru.radiationx.anilibria.navigation.BaseAppScreen
 import ru.radiationx.anilibria.navigation.Screens
+import ru.radiationx.anilibria.presentation.checker.CheckerPresenter
+import ru.radiationx.anilibria.presentation.checker.CheckerView
 import ru.radiationx.anilibria.presentation.main.MainPresenter
 import ru.radiationx.anilibria.presentation.main.MainView
 import ru.radiationx.anilibria.ui.activities.BaseActivity
+import ru.radiationx.anilibria.ui.activities.updatechecker.UpdateCheckerActivity
 import ru.radiationx.anilibria.ui.common.BackButtonListener
 import ru.radiationx.anilibria.ui.common.IntentHandler
 import ru.radiationx.anilibria.utils.DimensionHelper
@@ -40,7 +52,7 @@ import javax.inject.Inject
 import kotlin.math.max
 
 
-class MainActivity : BaseActivity(), MainView {
+class MainActivity : BaseActivity(), MainView, CheckerView {
 
     companion object {
         private const val TABS_STACK = "TABS_STACK"
@@ -84,6 +96,13 @@ class MainActivity : BaseActivity(), MainView {
     @ProvidePresenter
     fun provideMainPresenter(): MainPresenter = getDependency(MainPresenter::class.java)
 
+
+    @InjectPresenter
+    lateinit var checkerPresenter: CheckerPresenter
+
+    @ProvidePresenter
+    fun provideCheckerPresenter(): CheckerPresenter = getDependency(CheckerPresenter::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         injectDependencies()
         currentAppTheme = appThemeHolder.getTheme()
@@ -122,7 +141,56 @@ class MainActivity : BaseActivity(), MainView {
                 }
             }
         }
+        checkerPresenter.forceLoad = true
         Log.e("S_DEF_LOG", "main oncreate")
+    }
+
+
+    override fun setRefreshing(refreshing: Boolean) {}
+
+    override fun showUpdateData(update: UpdateData) {
+        val currentVersionCode = BuildConfig.VERSION_CODE
+
+        if (update.code > currentVersionCode) {
+            val context: Context = App.instance
+            val channelId = "anilibria_channel_updates"
+            val channelName = "Обновления"
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+                val manager = context.getSystemService(NotificationManager::class.java)
+                manager?.createNotificationChannel(channel)
+            }
+
+            val mBuilder = NotificationCompat.Builder(context, channelId)
+
+            val mNotificationManager = NotificationManagerCompat.from(context)
+
+            mBuilder.setSmallIcon(R.drawable.ic_notify)
+            mBuilder.color = ContextCompat.getColor(context, R.color.alib_red)
+
+            mBuilder.setContentTitle("Обновление AniLibria")
+            mBuilder.setContentText("Новая версия: ${update.name}")
+            mBuilder.setChannelId(channelId)
+
+
+            val notifyIntent = Intent(context, UpdateCheckerActivity::class.java)
+            notifyIntent.action = Intent.ACTION_VIEW
+            val notifyPendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, 0)
+            mBuilder.setContentIntent(notifyPendingIntent)
+
+            mBuilder.setAutoCancel(true)
+
+            mBuilder.priority = NotificationCompat.PRIORITY_DEFAULT
+            mBuilder.setCategory(NotificationCompat.CATEGORY_EVENT)
+
+            var defaults = 0
+            defaults = defaults or NotificationCompat.DEFAULT_SOUND
+            defaults = defaults or NotificationCompat.DEFAULT_VIBRATE
+            mBuilder.setDefaults(defaults)
+
+            mNotificationManager.notify(update.code, mBuilder.build())
+        }
     }
 
     override fun changeTheme(appTheme: AppThemeHolder.AppTheme) {

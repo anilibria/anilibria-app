10598c86140503d4eb141a5555b907ecb36ccfd0
Evgeny Nizamiev <radiationx@yandex.ru>
2019-03-25 21:36:17 +0500

    fix kitkat gradient

--- app/src/main/java/ru/radiationx/anilibria/ui/adapters/feed/FeedScheduleDelegate.kt
+++ app/src/main/java/ru/radiationx/anilibria/ui/adapters/feed/FeedScheduleDelegate.kt
@@ -1,5 +1,7 @@
 package ru.radiationx.anilibria.ui.adapters.feed
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.support.v4.view.ViewCompat
 import android.support.v7.widget.RecyclerView
 import android.view.View
@@ -8,6 +10,7 @@ import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_feed_schedule.*
 import ru.radiationx.anilibria.R
 import ru.radiationx.anilibria.entity.app.feed.ScheduleItem
+import ru.radiationx.anilibria.extension.dpToPx
 import ru.radiationx.anilibria.extension.visible
 import ru.radiationx.anilibria.ui.adapters.FeedScheduleListItem
 import ru.radiationx.anilibria.ui.adapters.ListItem
@@ -38,11 +41,23 @@ class FeedScheduleDelegate(
             containerView.setOnClickListener {
                 clickListener.invoke(currentItem, item_image)
             }
+            val gradientDrawable = GradientDrawable(
+                    GradientDrawable.Orientation.TOP_BOTTOM,
+                    intArrayOf(
+                            Color.parseColor("#99ffffff"),
+                            Color.parseColor("#99ffffff"),
+                            Color.parseColor("#00ffffff")
+                    )
+            ).also {
+                it.cornerRadius = 0f
+                it.gradientRadius = item_complete.dpToPx(20).toFloat()
+                it.gradientType = GradientDrawable.RADIAL_GRADIENT
+            }
+            item_complete.background = gradientDrawable
         }
 
         fun bind(item: ScheduleItem) {
             currentItem = item
-
             item_complete.visible(item.completed)
             ViewCompat.setTransitionName(item_image, "${item.javaClass.simpleName}_${item.releaseItem.id}")
             ImageLoader.getInstance().displayImage(item.releaseItem.poster, item_image)

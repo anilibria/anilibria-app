60d1dd9e65f4cf28eb4160b0b1fe01776f12d65d
Evgeny Nizamiev <radiationx@yandex.ru>
2019-08-11 17:28:57 +0500

    fix time format

--- app/src/main/java/ru/radiationx/anilibria/extension/Date.kt
+++ app/src/main/java/ru/radiationx/anilibria/extension/Date.kt
@@ -10,12 +10,17 @@ private object DateFormats {
     val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
     val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
     val timeSecFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+    val timeHourMinuteSecFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
 }
 
 fun Date.asDateTimeString(): String = DateFormats.dateTimeFormat.format(this)
 fun Date.asDateString(): String = DateFormats.dateFormat.format(this)
 fun Date.asTimeString(): String = DateFormats.timeFormat.format(this)
-fun Date.asTimeSecString(): String = DateFormats.timeSecFormat.format(this)
+fun Date.asTimeSecString(): String = if (time >= TimeUnit.HOURS.toMillis(1)) {
+    DateFormats.timeHourMinuteSecFormat.format(this)
+} else {
+    DateFormats.timeSecFormat.format(this)
+}
 
 fun Long.asUtcTime(): Long = this - TimeZone.getDefault().rawOffset / 1000
 fun Long.asUtc(): Long = this - TimeZone.getDefault().rawOffset

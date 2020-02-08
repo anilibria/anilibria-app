b2f982d70c60ad6669370700e9733f2fa1a59e87
Evgeny Nizamiev <radiationx@yandex.ru>
2020-02-09 00:50:32 +0700

    smoll changs

--- app/src/main/java/ru/radiationx/anilibria/di/providers/ApiNetworkClient.kt
+++ app/src/main/java/ru/radiationx/anilibria/di/providers/ApiNetworkClient.kt
@@ -6,7 +6,7 @@ import ru.radiationx.data.system.Client
 import javax.inject.Inject
 
 class ApiNetworkClient @Inject constructor(
-        private val clientWrapper: ru.radiationx.anilibria.di.providers.ApiClientWrapper,
+        private val clientWrapper: ApiClientWrapper,
         private val appCookieJar: AppCookieJar,
         private val apiConfig: ApiConfig
 ) : Client(clientWrapper, appCookieJar, apiConfig)
--- app/src/main/java/ru/radiationx/anilibria/di/providers/MainNetworkClient.kt
+++ app/src/main/java/ru/radiationx/anilibria/di/providers/MainNetworkClient.kt
@@ -6,7 +6,7 @@ import ru.radiationx.data.system.Client
 import javax.inject.Inject
 
 class MainNetworkClient @Inject constructor(
-        private val clientWrapper: ru.radiationx.anilibria.di.providers.MainClientWrapper,
+        private val clientWrapper: MainClientWrapper,
         private val appCookieJar: AppCookieJar,
         private val apiConfig: ApiConfig
 ) : Client(clientWrapper, appCookieJar, apiConfig)

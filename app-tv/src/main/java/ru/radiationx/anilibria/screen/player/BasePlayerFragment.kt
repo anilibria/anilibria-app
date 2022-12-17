417a2de9bde67bc0f9047178aa0972280b6886ff
RadiationX <radiationx@yandex.ru>
2022-12-17 22:16:53 +0500

    [TR-274] refactor: tv fix player controls hiding

--- app-tv/src/main/java/ru/radiationx/anilibria/screen/player/BasePlayerFragment.kt
+++ app-tv/src/main/java/ru/radiationx/anilibria/screen/player/BasePlayerFragment.kt
@@ -1,7 +1,9 @@
 package ru.radiationx.anilibria.screen.player
 
+import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import androidx.leanback.app.VideoSupportFragment
@@ -9,6 +11,7 @@ import androidx.leanback.app.VideoSupportFragmentGlueHost
 import androidx.leanback.widget.ArrayObjectAdapter
 import androidx.leanback.widget.ClassPresenterSelector
 import androidx.leanback.widget.ListRow
+import androidx.lifecycle.lifecycleScope
 import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -26,6 +29,8 @@ import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import dev.rx.tvtest.cust.CustomListRowPresenter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 open class BasePlayerFragment : VideoSupportFragment() {
 
@@ -45,12 +50,25 @@ open class BasePlayerFragment : VideoSupportFragment() {
     private val hlsMediaSourceFactory by lazy { HlsMediaSource.Factory(dataSourceFactory) }
     private val otherMediaSourceFactory by lazy { ProgressiveMediaSource.Factory(dataSourceFactory) }
 
+    @SuppressLint("RestrictedApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         initializePlayer()
         initializeRows()
+
+
+        fadeCompleteListener = object : OnFadeCompleteListener() {
+
+            override fun onFadeInComplete() {
+                super.onFadeInComplete()
+                // workaround for hiding controls when user click "enter"
+                isControlsOverlayAutoHideEnabled = false
+                isControlsOverlayAutoHideEnabled = true
+            }
+        }
     }
+
 
     override fun onPause() {
         super.onPause()
@@ -112,6 +130,7 @@ open class BasePlayerFragment : VideoSupportFragment() {
         val playerGlue = VideoPlayerGlue(requireContext(), playerAdapter).apply {
             host = VideoSupportFragmentGlueHost(this@BasePlayerFragment)
         }
+        Log.d("kekeke", "keks ${playerGlue.isControlsOverlayAutoHideEnabled}")
 
         this.player = player
         this.playerGlue = playerGlue

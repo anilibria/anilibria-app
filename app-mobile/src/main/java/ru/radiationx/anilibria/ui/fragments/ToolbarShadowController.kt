3d3aa860ab9d9c18be87163a766b0b4b09175fbc
RadiationX <radiationx@yandex.ru>
2023-10-14 17:53:16 +0500

    fix: feed crash by toolbar binding

--- app-mobile/src/main/java/ru/radiationx/anilibria/ui/fragments/ToolbarShadowController.kt
+++ app-mobile/src/main/java/ru/radiationx/anilibria/ui/fragments/ToolbarShadowController.kt
@@ -1,31 +1,43 @@
 package ru.radiationx.anilibria.ui.fragments
 
+import androidx.core.view.doOnAttach
+import androidx.core.view.doOnDetach
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.AppBarLayout
 
 open class ToolbarShadowController(
-        protected val recyclerView: RecyclerView,
-        protected val appBarLayout: AppBarLayout? = null,
-        protected val visibleListener: (Boolean) -> Unit
+    protected val recyclerView: RecyclerView,
+    protected val appBarLayout: AppBarLayout? = null,
+    protected val visibleListener: (Boolean) -> Unit,
 ) {
+
+    private val scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            listScrollY = recyclerView.computeVerticalScrollOffset()
+            updateToolbarShadow()
+        }
+    }
+
+    private val offsetListener = AppBarLayout.OnOffsetChangedListener { _, i ->
+        appBarOffset = i
+        updateToolbarShadow()
+    }
 
     protected var listScrollY = 0
     protected var appBarOffset = 0
     protected var lastVisible = false
 
     init {
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                listScrollY = recyclerView.computeVerticalScrollOffset()
-                updateToolbarShadow()
-            }
-        })
+        recyclerView.doOnAttach {
+            recyclerView.addOnScrollListener(scrollListener)
+            appBarLayout?.addOnOffsetChangedListener(offsetListener)
+        }
 
-        appBarLayout?.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { _, i ->
-            appBarOffset = i
-            updateToolbarShadow()
-        })
+        recyclerView.doOnDetach {
+            recyclerView.removeOnScrollListener(scrollListener)
+            appBarLayout?.removeOnOffsetChangedListener(offsetListener)
+        }
     }
 
     open fun updateToolbarShadow() {
